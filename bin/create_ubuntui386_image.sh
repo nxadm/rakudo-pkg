6b98ce4c9a389afba75b71894059d10ca7a600dc
u0097636@kuleuven.be <claudio.ramirez@kuleuven.be>
2016-09-02 15:46:23 +0200

    Move OS version to tag in new repo

--- bin/create_ubuntui386_image.sh
+++ bin/create_ubuntui386_image.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
 # Image to be pushed to docker hub as a base image for Ubuntu 16.04 i386 packages
-curl http://cdimage.ubuntu.com/ubuntu-base/releases/16.04/release/ubuntu-base-16.04-core-i386.tar.gz | gunzip | sudo docker import - nxadm/ubuntu16.04-i386
+# Use sudo if appropiate
+
+VERSION=$1
+if [ -z "$VERSION" ]; then
+    echo "Supply the Ubuntu version as a parameter"
+    exit 1
+fi
+BASEURL="http://cdimage.ubuntu.com/ubuntu-base/releases/$VERSION/release"
+FILE="ubuntu-base-${VERSION}-core-i386.tar.gz"
+curl "$BASEURL/$FILE" | gunzip | docker import - nxadm/ubuntu-i386:$VERSION
