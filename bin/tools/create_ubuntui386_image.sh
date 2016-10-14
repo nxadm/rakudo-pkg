ffb681f665aef355833c93de4118a57970f85a68
nxadm <pub.claudio@gmail.com>
2016-10-14 13:41:20 +0200

    Add support for Ubuntu 16.10

--- bin/tools/create_ubuntui386_image.sh
+++ bin/tools/create_ubuntui386_image.sh
@@ -2,12 +2,12 @@
 # Image to be pushed to docker hub as a base image for Ubuntu 16.04 i386 
 # packages.
 # Use sudo if appropiate
-
+set -xv
 VERSION=$1
 if [ -z "$VERSION" ]; then
     echo "Supply the Ubuntu version as a parameter"
     exit 1
 fi
 BASEURL="http://cdimage.ubuntu.com/ubuntu-base/releases/$VERSION/release"
-FILE="ubuntu-base-${VERSION}-core-i386.tar.gz"
+FILE="ubuntu-base-${VERSION}-base-i386.tar.gz"
 curl "$BASEURL/$FILE" | gunzip | docker import - nxadm/ubuntu-i386:$VERSION
