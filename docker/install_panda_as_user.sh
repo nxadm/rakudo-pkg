034bbfc4db4adcf1fe0cb0907c77162a234e2966
nxadm <pub.claudio@gmail.com>
2016-10-14 15:56:18 +0200

    Fix zef install as local user

--- docker/install_panda_as_user.sh
+++ docker/install_panda_as_user.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # Perl6 Module Management (https://github.com/tadzik/panda)
 # You need Git in order to install and use Panda.
@@ -8,5 +8,8 @@ git clone --recursive git://github.com/tadzik/panda.git
 cd panda
 /opt/rakudo/bin/perl6 bootstrap.pl
 rm -rf /var/tmp/panda
+
+echo "panda has been installed to ~/.perl6/bin."
+echo "Add ~/.perl6/bin to your PATH."
 
 exit 0
--- docker/install_zef_as_user.sh
+++ docker/install_zef_as_user.sh
@@ -6,7 +6,10 @@
 cd /var/tmp
 git clone https://github.com/ugexe/zef.git
 cd zef
-/opt/rakudo/bin/perl6 -Ilib bin/zef install .
+/opt/rakudo/bin/perl6 -Ilib bin/zef --install-to=home install .
 rm -rf /var/tmp/zef
+
+echo "zef has been installed to ~/.perl6/bin."
+echo "Add ~/.perl6/bin to your PATH."
 
 exit 0
