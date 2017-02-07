4cd30e54066bb4a9e952bbb5bd9e7af1205536a5
nxadm <pub.claudio@gmail.com>
2017-02-07 15:31:23 +0100

    Remove panda and add zef as root install

--- docker/install_zef_as_user.sh
+++ docker/install_zef_as_user.sh
@@ -4,12 +4,14 @@
 # You need Git in order to install and use Zef.
 
 cd /var/tmp
-git clone https://github.com/ugexe/zef.git
-cd zef
+rm -rf /var/tmp/zef_$USER
+git clone https://github.com/ugexe/zef.git zef_$USER
+cd zef_$USER
 /opt/rakudo/bin/perl6 -Ilib bin/zef --install-to=home install .
-rm -rf /var/tmp/zef
+rm -rf /var/tmp/zef_$USER
 
 echo "zef has been installed to ~/.perl6/bin."
-echo "Add ~/.perl6/bin to your PATH."
+echo "Add /opt/rakudo/bin and ~/.perl6/bin to your PATH, e.g.:"
+echo "echo 'export PATH=/opt/rakudo/bin:~/.perl6/bin:$PATH' >> ~/.bash_profile"
 
 exit 0
