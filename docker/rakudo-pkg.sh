c0b059e4209a8c9bdac24a41867f7dcd96fcec8e
Patrick Böker <6307078+patzim@users.noreply.github.com>
2019-03-01 12:08:15 +0100

    Do not spam $PATH
    On some systems (Fedora) this is called from .bashrc and thus each time a shell is opened. This change prevents adding the path multiple times in such a case.

--- docker/rakudo-pkg.sh
+++ docker/rakudo-pkg.sh
@@ -1,2 +1,4 @@
-PATH=$PATH:/opt/rakudo-pkg/bin:/opt/rakudo-pkg/share/perl6/bin
-export PATH
+RAKUDO_PATHS=/opt/rakudo-pkg/bin:/opt/rakudo-pkg/share/perl6/bin
+if ! echo "$PATH" | /bin/grep -Eq "(^|:)$RAKUDO_PATHS($|:)" ; then
+    export PATH="$PATH:$RAKUDO_PATHS"
+fi
