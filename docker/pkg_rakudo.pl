326e76face172932e2921d5e3358fd6cc95e2ec1
Claudio Ramirez <pub.claudio@gmail.com>
2020-02-25 20:44:12 +0100

    and a newer one

--- docker/pkg_rakudo.pl
+++ docker/pkg_rakudo.pl
@@ -95,10 +95,12 @@ sub build {
         my $cmd = join(' ', @configure);
         system('sh', '-c', "CFLAGS='-fPIC -DDL_USE_GLIBC_ITER_PHDR' $cmd") == 0
             or return 0;
+        system('sh', '-c', "CFLAGS='-fPIC -DDL_USE_GLIBC_ITER_PHDR' make") == 0
+            or return 0;
     } else {
         system(@configure) == 0 or return 0;
+        system('make')     == 0 or return 0;
     }
-    system('make')     == 0 or return 0;
     # make test
     if (!$skip_tests) {
         system('make', 'test') == 0 or return 0;
