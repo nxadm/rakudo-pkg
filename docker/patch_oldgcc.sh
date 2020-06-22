7b9107d94182749319c60d4ab430efcad8022f0c
Claudio Ramirez <pub.claudio@gmail.com>
2020-06-22 18:37:16 +0200

    patch script

---
+++ docker/patch_oldgcc.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/perl 
+perl -pi -e "s/\bccmiscflags\w+=>\w+'-Wextra\b/ccmiscflags  => '-std=gnu99 -Wextra" build/setup.pm
--- docker/pkg_rakudo.pl
+++ docker/pkg_rakudo.pl
@@ -94,7 +94,7 @@ sub build {
         if (($os eq 'CentOS' && $os_release eq '7') ||
            ($os eq 'Debian' && $os_release eq '8')) {
             say "Patching moarvm for older gcc";
-            system("perl -pi -e 's/\\bccmiscflags\\w+=>\\w+\\'-Wextra\b/ccmiscflags  => \\'-std=gnu99 -Wextra/' build/setup.pm");
+            system('/patch_oldgcc.sh') == 0 or return 0;
             system('grep "std=gnu99" build/setup.pm');
         }
     }
