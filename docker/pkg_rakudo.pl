9f6a16e1a17f5a39d04f9da78eefc4a752f8a180
Claudio Ramirez <pub.claudio@gmail.com>
2020-06-22 18:06:39 +0200

    escapes

--- docker/pkg_rakudo.pl
+++ docker/pkg_rakudo.pl
@@ -94,8 +94,8 @@ sub build {
         if (($os eq 'CentOS' && $os_release eq '7') ||
            ($os eq 'Debian' && $os_release eq '8')) {
             say "Patching moarvm for older gcc";
-            system('perl -pi -e "s/\bccmiscflags\w+=>\w+\'-Wextra\b/ccmiscflags  => \'-std=gnu99 -Wextra/" build/setup.pm') == 0 or return 0;
-            system('grep "std=gnu99" build/setup.pm') == 0 or return 0;
+            system("perl -pi -e 's/\\bccmiscflags\\w+=>\\w+\\'-Wextra\b/ccmiscflags  => \\'-std=gnu99 -Wextra/' build/setup.pm");
+            system('grep "std=gnu99" build/setup.pm');
         }
     }
 
