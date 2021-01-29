3c59f87b20a5782f2bca61a40e0a1f00ded03d97
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-29 22:37:30 +0100

    test

--- actions/02-build.sh
+++ actions/02-build.sh
@@ -30,32 +30,3 @@ cd zef
 $INSTALL_ROOT/bin/raku -I. bin/zef --install-to=core install .
 ln -s $INSTALL_ROOT/share/perl6/core/bin/zef $INSTALL_ROOT/bin/zef
 zef --version
-
-ls -la $INSTALL_ROOT/bin
-
-find $INSTALL_ROOT
-
-# Create links and add scripts
-#cd $INSTALL_ROOT/bin
-#rm raku raku-debug
-#ln -s rakudo raku
-#ln -s rakudo-debug raku-debug
-#ln -s ../share/perl6/site/bin/zef .
-#cp /install-zef-as-user .
-#cp /fix-windows10 .
-#cp /add-rakudo-to-path .
-#
-## Package it in /mnt
-#cd /
-#mv $INSTALL_ROOT rakudo-pkg-$RAKUDO_VERSION
-#mkdir -p /staging
-#TARGZ="rakudo-pkg-mooarvm-${RAKUDO_VERSION}-${REVISION}-linux-${ARCH}.tar.gz"
-#tar -czf /staging/$TARGZ rakudo-pkg-$RAKUDO_VERSION
-#
-## Checksum it
-#cd /staging
-#sha1sum $TARGZ > "${TARGZ}.sha1"
-#echo "Package checksum:"
-#cat "${TARGZ}.sha1"
-#
-#exit 0
