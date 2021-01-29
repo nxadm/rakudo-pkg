add9d0ef20c62787c073ca5e9fd9b725d4fb5ed1
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-29 01:14:27 +0100

    test

--- build_env.sh
+++ build_env.sh
@@ -4,5 +4,5 @@ set -xv
 if [ `grep alpine /etc/os-release | wc -l` > 0 ]; then
 	apk add build-base perl perl-utils gzip tar
     export OS=alpine
-    export OS_VERSIO=`grep "VERSION_ID=" /etc/os-release | cut -d= -f2`
+    export OS_VERSION=`grep "PRETTY_NAME=" /etc/os-release | cut -dv -f2`
 fi
