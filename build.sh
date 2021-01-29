eae25c843eed1e88c014421ecbd85e2dd05e8d61
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-29 19:11:23 +0100

    test

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -e
+
+# Build rakudo
+for i in moarvm nqp rakudo; do
+    mkdir $i
+    tar xzf $i.tar.gz -C $i --strip-components=1
+    CONFIGURE="perl ./Configure.pl --prefix=$INSTALL_ROOT"
+    if [ $soft != "MOARVM" ]; then
+        CONFIGURE+=" --backends=moar"
+    fi 
+
+    cd $i
+    $CONFIGURE
+    make
+    make test
+    make install
+    cd ..
+done    
+
+# Install zef
+mkdir zef
+tar xzf zef.tar.gz -C zef --strip-components=1
+cd zef
+$INSTALL_ROOT/bin/raku -I. bin/zef --install-to=core install .
+
+## Create links and add scripts
+#cd $INSTALL_ROOT/bin
+#rm raku raku-debug
+#ln -s rakudo raku
+#ln -s rakudo-debug raku-debug
+#ln -s ../share/perl6/site/bin/zef .
+#cp /install-zef-as-user .
+#cp /fix-windows10 .
+#cp /add-rakudo-to-path .
+#
+## Package it in /mnt
+#cd /
+#mv $INSTALL_ROOT rakudo-pkg-$RAKUDO_VERSION
+#mkdir -p /staging
+#TARGZ="rakudo-pkg-mooarvm-${RAKUDO_VERSION}-${REVISION}-linux-${ARCH}.tar.gz"
+#tar -czf /staging/$TARGZ rakudo-pkg-$RAKUDO_VERSION
+#
+## Checksum it
+#cd /staging
+#sha1sum $TARGZ > "${TARGZ}.sha1"
+#echo "Package checksum:"
+#cat "${TARGZ}.sha1"
+#
+#exit 0
