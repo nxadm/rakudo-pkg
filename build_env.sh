c39aee6a2bec0aae9bf4c5f93522b5f51583d2ff
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-29 01:01:18 +0100

    "test'

--- build_env.sh
+++ build_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -xv
 
-if [ `grep -i alpine /etc/*release` ]; then
+if [ `grep -i alpine /etc/*release | wc -l` > 0 ]; then
     echo "Alpine!"
 	apk add build-base perl perl-utils gzip tar
 fi
