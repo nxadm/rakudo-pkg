32d67deed86ddda86fb514807a1c3138d28549b7
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-29 11:49:03 +0100

    test

--- build_env.sh
+++ build_env.sh
@@ -1,12 +1,17 @@
 #!/bin/sh -e
 set -xv
 
+set_os_vars() {
+    OS=$1
+    OS_VERSION=`perl -lwn -e 'if (/PRETTY_NAME/) { s/^.+\sv*([\w\d.]+)\b.+/$1/; print } ' /etc/os-release`
+    OS_CODENAME=`perl -lwn -e 'if (/VERSION_CODENAME/) { s/^.+=(.+)/$1/; print } ' /etc/os-release`
+}
+
 if [ `grep ^ID=alpine /etc/os-release` ]; then
-    OS=alpine
-    OS_VERSION=`grep "PRETTY_NAME=" /etc/os-release | cut -dv -f2`
     apk update
     apk upgrade
 	apk add build-base perl perl-utils gzip tar
+    set_os_vars(alpine)
 fi
 
 if [ `grep ^ID=debian /etc/os-release` ]; then
@@ -18,18 +23,16 @@ if [ `grep ^ID=ubuntu /etc/os-release` ]; then
 fi
 
 if [ "$OS" = "debian" ] || [ "$OS" = "ubuntu" ]; then
-    OS_VERSION=`grep ^VERSION_ID= /etc/os-release | cut -d= -f2 | cut -d\" -f2`
-    OS_CODENAME=`grep ^VERSION_CODENAME= /etc/os-release | cut -d= -f2`
     apt-get update
     apt-get -u dist-upgrade -y -qq
     apt-get install -y build-essential
+    OS_VERSION=`grep ^VERSION_ID= /etc/os-release | cut -d= -f2 | cut -d\" -f2`
+    OS_CODENAME=`grep ^VERSION_CODENAME= /etc/os-release | cut -d= -f2`
 fi
 
 
 echo export OS=$OS >> versions.sh 
 echo export OS_VERSION=$OS_VERSION >> versions.sh
-if [ ! -z $OS_CODENAME ]; then
-    echo export OS_CODENAME=$OS_CODENAME >> versions.sh
-fi
+echo export OS_CODENAME=$OS_CODENAME >> versions.sh
 cat versions.sh >> /etc/profile
 
