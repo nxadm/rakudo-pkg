99e46634a7419b6aa0340c22d70027da76cdb662
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-29 02:03:00 +0100

    test

--- build_env.sh
+++ build_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 set -xv
 
-if [ `grep ID=alpine /etc/os-release | wc -l` > 0 ]; then
+if [ `grep ^ID=alpine /etc/os-release | wc -l` > 0 ]; then
     OS=alpine
     OS_VERSION=`grep "PRETTY_NAME=" /etc/os-release | cut -dv -f2`
     apk update
@@ -9,15 +9,27 @@ if [ `grep ID=alpine /etc/os-release | wc -l` > 0 ]; then
 	apk add build-base perl perl-utils gzip tar
 fi
 
-if [ `grep ID=debian /etc/os-release | wc -l` > 0 ]; then
+if [ `grep ^ID=debian /etc/os-release | wc -l` > 0 ]; then
     OS=debian
+fi
+
+if [ `grep ^ID=ubuntu /etc/os-release | wc -l` > 0 ]; then
+    OS=ubuntu
+fi
+
+if [ $OS == "debian" || $OS == "ubuntu"  ]; then
     OS_VERSION=`grep "VERSION_ID=" /etc/os-release | cut -d= -f2`
+    OS_CODENAME=`grep "VERSION_CODENAME=" /etc/os-release | cut -d= -f2`
     apt-get update
     apt-get -u dist-upgrade -y -qq
     apt-get install -y build-essential
 fi
 
-cat "export OS=$OS" >> /etc/profile 
-cat "export OS_VERSION=$OS_VERSION" >> /etc/profile 
+
+echo "export OS=$OS" >> /etc/profile 
+echo "export OS_VERSION=$OS_VERSION" >> /etc/profile 
+if [ ! -z $OS_CODENAME ]; then
+    echo "export OS_CODENAME=$OS_CODENAME" >> /etc/profile 
+fi
 cat versions.sh >> /etc/profile
 
