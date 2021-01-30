7b38aeca00df8a5a05137048e88ad38da23b0de4
Claudio Ramirez <pub.claudio@gmail.com>
2021-01-30 09:14:14 +0100

    test

--- actions/01-setup_env.sh
+++ actions/01-setup_env.sh
@@ -21,36 +21,36 @@ case "$OS" in
     alpine)
         apk update
         apk upgrade
-	    apk add bash build-base gzip perl perl-utils tar
+	    apk add bash build-base gzip perl perl-utils tar zstd-dev
         ;;
     centos)
         yum -q -y upgrade
         yum -q -y groupinstall 'Development Tools'
-        yum -q -y install perl-core
+        yum -q -y install libzstd-devel perl-core
         ;;
     debian)
         apt-get update
         apt-get -u dist-upgrade -y -qq
-        apt-get install -y build-essential
+        apt-get install -y build-essential libzstd-dev
         ;;
     fedora)
         dnf -q -y upgrade
         dnf -q -y groupinstall 'Development Tools'
-        dnf -q -y install perl-core
+        dnf -q -y install libzstd-devel perl-core
         ;;
     opensuse)
        zypper refresh
        zypper update -y
-       zypper install -y gcc gzip make perl tar
+       zypper install -y gcc gzip make libzstd-devel perl tar
         ;;
     rhel)
         microdnf update
-        microdnf install gcc gzip hostname make perl-core tar
+        microdnf install gcc gzip libzstd-devel make perl-core tar
         ;;
     ubuntu)
         apt-get update
         apt-get -u dist-upgrade -y -qq
-        apt-get install -y build-essential git
+        apt-get install -y build-essential git libzstd-dev
         ;;
     *)
         echo "Sorry, distro not found. Send a PR. :)"
