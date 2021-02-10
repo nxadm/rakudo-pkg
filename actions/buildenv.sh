49180c5f5458e7e4b1021f7d7507b748ef4d9aee
Claudio Ramirez <pub.claudio@gmail.com>
2021-02-10 21:04:48 +0100

    debug

--- actions/buildenv.sh
+++ actions/buildenv.sh
@@ -28,7 +28,7 @@ set_os_vars() {
   echo ARCH=$ARCH >> config/setup.sh
   echo OS=$OS >> config/setup.sh
   if [ ! -z "$RUN_DEPS" ]; then
-    echo RUN_DEPS="- $RUN_DEPS" >> config/setup.sh
+    echo RUN_DEPS="\"- $RUN_DEPS\"" >> config/setup.sh
   fi
   echo OS_VERSION=$OS_VERSION >> config/setup.sh
   echo OS_CODENAME=$OS_CODENAME >> config/setup.sh
