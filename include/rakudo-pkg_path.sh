14a70afcf4742f731968f4d6788b334d44d774e0
nxadm <nxadm@users.noreply.github.com>
2021-02-12 10:22:59 +0100

    no output

--- include/rakudo-pkg_path.sh
+++ include/rakudo-pkg_path.sh
@@ -1,5 +1,4 @@
-set -xv
-# Add this to /etc/profile.d/
+# To be run in /etc/profile.d/
 RAKUDO_PATHS=/opt/rakudo-pkg/bin:/opt/rakudo-pkg/share/perl6/bin
 if ! echo "$PATH" | /bin/grep -Eq "(^|:)$RAKUDO_PATHS($|:)" ; then
     export PATH="$PATH:$RAKUDO_PATHS"
