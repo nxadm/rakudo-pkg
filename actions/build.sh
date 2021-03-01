3d0f1a52e12972a1aee8383dd4e8fb946ce873bd
Claudio Ramirez <pub.claudio@gmail.com>
2021-03-01 15:40:01 +0100

    fix bug #78

--- actions/build.sh
+++ actions/build.sh
@@ -56,6 +56,10 @@ raku -v
 mkdir -p $INSTALL_ROOT/var/zef
 tar xzf zef.tar.gz -C $INSTALL_ROOT/var/zef --strip-components=1
 
+# Workaround ugly rakudo/zef bug
+# https://github.com/nxadm/rakudo-pkg/issues/78
+mkdir -p $INSTALL_ROOT/share/perl6/site/short
+
 # Add extra scripts and directories
 cd include
 chmod +x *
