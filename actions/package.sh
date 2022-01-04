b7ec722cc3328e5af34a9cb98a2a75e7ab8e9d60
Claudio Ramirez <pub.claudio@gmail.com>
2022-01-04 10:51:29 +0100

    abstract DISTRO_ARCH

--- actions/package.sh
+++ actions/package.sh
@@ -12,47 +12,56 @@ mv nfpm /usr/bin
 envsubst < config/nfpm.yaml > config/nfpm.yaml_tmp
 mv config/nfpm.yaml_tmp config/nfpm.yaml
 
+DISTRO_ARCH=$ARCH
+if [ "$OS" = "alpine" -o "$OS" = "el" -o "$OS" = "fedora" -o "$OS" = "opensuse" ]; then
+  if [ $ARCH = "amd64" ]; then
+    DISTRO_ARCH=x86_64
+  else
+    DISTRO_ARCH=aarch64
+  fi
+fi
+
 # Package
 case "$OS" in
     alpine)
         PACKAGER=apk
         INSTALL_CMD="if [ ! -z "$INSTALL_DEPS" ]; then apk add $INSTALL_DEPS; fi; apk add --no-cache --allow-untrusted *.apk"
-        PKG_NAME="rakudo-pkg-Alpine${OS_VERSION}_${RAKUDO_VERSION}-${PKG_REVISION}_x86_64.apk"
+        PKG_NAME="rakudo-pkg-Alpine${OS_VERSION}_${RAKUDO_VERSION}-${PKG_REVISION}_${DISTRO_ARCH}.apk"
         PKG_CMD="cloudsmith push alpine $CLOUDSMITH_REPOSITORY/$OS/$OS_VERSION $PKG_NAME"
         ;;
     debian)
         PACKAGER=deb
-        PKG_NAME=rakudo-pkg-Debian${OS_VERSION}_${RAKUDO_VERSION}-${PKG_REVISION}_amd64.deb
+        PKG_NAME=rakudo-pkg-Debian${OS_VERSION}_${RAKUDO_VERSION}-${PKG_REVISION}_${DISTRO_ARCH}.deb
         INSTALL_CMD="apt-get update; apt install -y ./$PKG_NAME"
         PKG_CMD="cloudsmith push deb $CLOUDSMITH_REPOSITORY/$OS/$OS_CODENAME $PKG_NAME"
         ;;
     el)
         PACKAGER=rpm
         INSTALL_CMD="if [ ! -z "$INSTALL_DEPS" ]; then microdnf install $INSTALL_DEPS; fi; rpm -Uvh *.rpm"
-        PKG_NAME=rakudo-pkg-EL${OS_VERSION}-${RAKUDO_VERSION}-${PKG_REVISION}.x86_64.rpm
+        PKG_NAME=rakudo-pkg-EL${OS_VERSION}-${RAKUDO_VERSION}-${PKG_REVISION}.${DISTRO_ARCH}.rpm
         PKG_CMD="cloudsmith push rpm $CLOUDSMITH_REPOSITORY/$OS/$OS_VERSION $PKG_NAME"
         ;;
     fedora)
         PACKAGER=rpm
         INSTALL_CMD='dnf -y install *.rpm'
-        PKG_NAME=rakudo-pkg-Fedora${OS_VERSION}-${RAKUDO_VERSION}-${PKG_REVISION}.x86_64.rpm
+        PKG_NAME=rakudo-pkg-Fedora${OS_VERSION}-${RAKUDO_VERSION}-${PKG_REVISION}.${DISTRO_ARCH}.rpm
         PKG_CMD="cloudsmith push rpm $CLOUDSMITH_REPOSITORY/$OS/$OS_VERSION $PKG_NAME"
         ;;
     opensuse)
         PACKAGER=rpm
-        PKG_NAME=rakudo-pkg-openSUSE${OS_VERSION}-${RAKUDO_VERSION}-${PKG_REVISION}.x86_64.rpm
+        PKG_NAME=rakudo-pkg-openSUSE${OS_VERSION}-${RAKUDO_VERSION}-${PKG_REVISION}.${DISTRO_ARCH}.rpm
         INSTALL_CMD="if [ ! -z "$INSTALL_DEPS" ]; then zypper install -y $INSTALL_DEPS; fi; rpm -Uvh *.rpm"
         PKG_CMD="cloudsmith push rpm $CLOUDSMITH_REPOSITORY/$OS/$OS_VERSION $PKG_NAME"
         ;;
     ubuntu)
         if [ "${OS}${OS_VERSION}" = "$PKG_TARGZ" ]; then
             PACKAGER=targz
-            PKG_NAME=rakudo-pkg-linux-relocable-${RAKUDO_VERSION}-${PKG_REVISION}_${ARCH}.tar.gz
+            PKG_NAME=rakudo-pkg-linux-relocable-${RAKUDO_VERSION}-${PKG_REVISION}_${DISTRO_ARCH}.tar.gz
             INSTALL_CMD="tar xvzf /staging/$PKG_NAME"
             PKG_CMD="true"
         else
             PACKAGER=deb
-            PKG_NAME=rakudo-pkg-Ubuntu${OS_VERSION}_${RAKUDO_VERSION}-${PKG_REVISION}_amd64.deb
+            PKG_NAME=rakudo-pkg-Ubuntu${OS_VERSION}_${RAKUDO_VERSION}-${PKG_REVISION}_${DISTRO_ARCH}.deb
             INSTALL_CMD="apt-get update; apt install -y ./$PKG_NAME"
             PKG_CMD="cloudsmith push deb $CLOUDSMITH_REPOSITORY/$OS/$OS_CODENAME $PKG_NAME"
         fi
